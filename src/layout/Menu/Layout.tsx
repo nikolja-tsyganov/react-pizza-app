3e9e5a596c82a09e3d4b83c3f8a9172f44e82ff5
Nikolay Tsyganov <nikolja1990@gmail.com>
2026-06-17 12:02:36 +0400

    7 коммит

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,9 +1,16 @@
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
 import cn from 'classnames';
 
 export function Layout() {
+	const navigate = useNavigate();
+
+	const logout = () => {
+		localStorage.removeItem('jwt');
+		navigate('/auth/login');
+	}
+
 	return <div className={styles['layout']}>
 		<div className={styles['sidebar']}>
 			<div className={styles['user']}>
@@ -26,7 +33,7 @@ export function Layout() {
 					Корзина
 				</NavLink>
 			</div>
-			<Button className={styles['exit']} >
+			<Button className={styles['exit']} onClick={logout}>
 				<img src="/exit-icon.svg" alt="Выйти" />
 				Выйти
 			</Button>
